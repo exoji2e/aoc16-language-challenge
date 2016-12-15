0ad87232be65e49d4675bbc3a3bb1b6f51fa1c3b
Måns Magnusson <exoji2e@gmail.com>
2016-12-15 23:24:55 +0100

    Refactored A.fs

--- 6-f#/A.fs
+++ 6-f#/A.fs
@@ -5,10 +5,10 @@ let main() =
         Seq.initInfinite (fun _ -> Console.ReadLine())
         |> Seq.takeWhile(fun line -> line <> null)
         |> Seq.toList
+
     let all = 
         lines 
         |> List.fold(fun coll line -> List.append coll (List.zip [1..line.Length] (List.ofSeq line))) []
-    //printfn "%A" all
     
     let set =
         all
@@ -16,19 +16,13 @@ let main() =
         |> Seq.countBy (fun c -> c)
         |> Seq.sortBy (fun elem -> - snd elem)
         |> Seq.distinctBy (fun elem -> fst (fst elem))
-        |> Seq.sortBy (fun elem -> fst (fst elem))
 
     let str =
         set
+        |> Seq.sortBy (fun elem -> fst (fst elem))
         |> Seq.map (fun ((a,b),c) -> b)
         |> String.Concat
 
     Console.WriteLine(str)
     
-    (*for line in lines do 
-        let xs = List.zip [1..line.Length] (List.ofSeq line)
-        printfn "%A" xs
-        //Console.WriteLine(xs[1])
-    //printfn "%A" all 
-    *)
 main()
