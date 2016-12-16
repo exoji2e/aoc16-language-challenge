a353de29f2130d88bf2257adb80041609a810e85
Måns Magnusson <exoji2e@gmail.com>
2016-12-16 11:59:54 +0100

    Add day6 part2 in f#

--- 6-f#/A.fs
+++ 6-f#/A.fs
@@ -6,23 +6,29 @@ let main() =
         |> Seq.takeWhile(fun line -> line <> null)
         |> Seq.toList
 
-    let all = 
+    let set = 
         lines 
         |> List.fold(fun coll line -> List.append coll (List.zip [1..line.Length] (List.ofSeq line))) []
-    
-    let set =
-        all
         |> List.toSeq
         |> Seq.countBy (fun c -> c)
-        |> Seq.sortBy (fun elem -> - snd elem)
-        |> Seq.distinctBy (fun elem -> fst (fst elem))
-
-    let str =
+    
+    let str1 = //part1
         set
+        |> Seq.sortBy (fun elem -> - snd elem) //highest frequency
+        |> Seq.distinctBy (fun elem -> fst (fst elem))
+        |> Seq.sortBy (fun elem -> fst (fst elem))
+        |> Seq.map (fun ((a,b),c) -> b)
+        |> String.Concat
+        
+    let str2 = //part2
+        set
+        |> Seq.sortBy (fun elem -> snd elem) //lowest frequency
+        |> Seq.distinctBy (fun elem -> fst (fst elem))
         |> Seq.sortBy (fun elem -> fst (fst elem))
         |> Seq.map (fun ((a,b),c) -> b)
         |> String.Concat
 
-    Console.WriteLine(str)
+    Console.WriteLine(str1)
+    Console.WriteLine(str2)
     
 main()
